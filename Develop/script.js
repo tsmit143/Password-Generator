7f024f7622006d95187c2be573dc7ca80aa644cc
tsmit143 <148919767+tsmit143@users.noreply.github.com>
2023-12-03 17:08:53 -0500

    added password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,4 @@
 // Assignment code here
-/*
-//**Gathers the desired amount of password characters**
-//Make Prompt for length of password
-var lengthOption = prompt ("Enter password length: must be between 8 and 128 characters!");
-//Displays error message if too short
-if (lengthOption < 8) {
- alert ("Option request too short, try again");
-}
-// Displays error message if too 
-if (lengthOption > 128) {
- alert ("option resquest too long, try again");
-}
-console.log(lengthOption); //delete later
-
-//make prompt for character types -  lowercase, uppercase, numeric, and/or special characters
-//going to need an array for each option
-
-//prompt for instructions moving forward
-var promtInstructions = prompt ("Respond either [Yes or No] to the following prompts to selcet your password criteria options...select OK to continue!");
-
-var lowCase = prompt ("Lowercase: Yes or No");
-
-/*if (lowCase === "Yes"){
-  console.log("True");
-} else if (lowCase === "No"){
-  console.log("False");
-} else { alert ("Please respond with Yes or No");}*//*
-
-var answer = false;
-function criteriaQuestion (choice){
-  if (choice === "Yes"){
-    answer = true;
-  } else if (choice === "No"){ 
-    answer = false;
-  } else { alert ("Please respond with Yes or No");
-         }
-         return;
-}
-criteriaQuestion (lowCase);
-*/
-
-
-
-//display selected options options: lowercase, uppercase, numeric, and/or special characters.. if corret move fwd if not ask them to adjust options
-
-//Check type input return error if not one of the above
-//Return error if no option is selected
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -73,15 +23,15 @@ var big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var small = 'abcdefghijklmnopqrstuvwxyz';
 var digits = '123456789';
 var specials = '@#$!*';
-var criteriaOps = [0];
+var criteriaOps = [];
 
 //___________________lowercase prompt______________________
 var lowCase = confirm ("Select 'OK' if for Lowercase"); 
 
-if (lowCase === true) {//lowercase is confiremed and lowercase letter added to critieria array
+if (lowCase === true) { //lowercase is confiremed and lowercase letter added to critieria array
 for(var i=0; i < small.length; i++){
 //  alert ("Lowercase is confirmed!");
-  criteriaOps.push(small[i]);
+criteriaOps.push(small[i]);
  }
 } else if (lowCase === false){alert("Lowercase is denied");}//lowercase is denied and not added to critieria array
 
@@ -117,12 +67,13 @@ if (nums === true) {
   } else if (nums === false){alert("Lowercase is denied");}
   
   console.log(criteriaOps);
+//_____________________password generation_________________________
 
-var pass = '';//password varible
-
-for (var i=1; i <= lengthOption; i++){
-  var char = Math.floor(Math.random()* criteriaOps.length );
-  pass += criteriaOps.charAt(char)
+var pass = ''; //password varible
+for (var i = 1; i <= lengthOption; i++){
+  const randomIndex = Math.floor(Math.random() * criteriaOps.length);
+  console.log (criteriaOps[randomIndex]);
+ pass += criteriaOps[randomIndex];
 }
   return pass;
 }
